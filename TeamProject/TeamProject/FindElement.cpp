0c1686e7e48bf5d171f40587c26c8a1520783326
InPyFFiNess <bloodfrire@gmail.com>
2025-03-12 21:09:19 +0300

    Написал второе задание

--- TeamProject/TeamProject/FindElement.cpp
+++ TeamProject/TeamProject/FindElement.cpp
@@ -1,6 +1,18 @@
 #include "MainHeader.h"
+#include <iostream>
 
-void FindElement(int array, int size)
+void FindElement(int* array, int size)
 {
-
+	int element{};
+	int position{-1};
+	std::cout << "Input element: ";
+	std::cin >> element;
+	for (size_t i = 0; i < size; ++i)
+	{
+		if (array[i] == element)
+		{
+			position = i;
+		}
+	}
+	std::cout << position;
 }
